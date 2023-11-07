268a5fc973bc0fb4ae373268f3a07288a51f0cfe
Menna271 <mennanagy271@gmail.com>
2023-11-07 23:30:39 +0200

    Snake effect using CMake

---
+++ build/CMakeFiles/snake_effect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake_effect.dir/src/main.c.obj"
+  "snake_effect.elf"
+  "snake_effect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/snake_effect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
